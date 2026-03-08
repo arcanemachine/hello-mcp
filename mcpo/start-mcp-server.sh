3a2c89efbb9723167b5f5e1f5e80a65c1dcfe66d
Nicholas Moen <arcanemachine@gmail.com>
2026-03-07 18:27:30 -0700

    chore: bug fixes and performance improvements

--- mcpo/start-mcp-server.sh
+++ mcpo/start-mcp-server.sh
@@ -1,3 +1,3 @@
 echo "WARNING: Starting MCP server on all interfaces (0.0.0.0) to work around a llama-server CORS issue..."
 sleep 1
-uvx mcpo --config config.local.json --host 0.0.0.0 --port 8000
+uvx mcpo --config config.local.json --host 0.0.0.0 --port 8000 --transport streamable-http
